0a86e0f48ab044d2fd7ac7a8402bef3dfb6152fe
Mikulas <rullaf@gmail.com>
2015-11-21 13:47:23 +0100

    assert phpdbg and working version

--- src/controller.php
+++ src/controller.php
@@ -16,6 +16,17 @@ array_shift($filesToRun);
 
 $control = new ProcessControl(getmypid(), 3);
 
+if (PHP_SAPI !== 'phpdbg') {
+	echo "Expected phpdbg SAPI, run as\n";
+	echo "  phpdbg -qrr " . implode(' ', array_map('escapeshellarg', $argv)) . "\n";
+	die;
+}
+
+if (version_compare(PHP_VERSION, '7.0.0RC7', '<')) {
+	echo "PHP " . PHP_VERSION . " is not supported, use at PHP >=7.0.0RC7.\n";
+	die;
+}
+
 while ($file = array_shift($filesToRun)) {
 	var_dumP("FILE", $file);
 	if ($control->fork() === ProcessControl::CHILD) {
