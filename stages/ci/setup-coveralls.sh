f30d8ec8640e9b8d23ce49a97436594ff8756276
Mikulas <rullaf@gmail.com>
2015-11-22 01:20:05 +0100

    configure converalls

--- stages/ci/setup-coveralls.sh
+++ stages/ci/setup-coveralls.sh
@@ -2,11 +2,9 @@
 set -uo pipefail
 IFS=$'\n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
-ROOT="$(dirname "$(dirname "$DIR")")"
 
 if [[ "$TRAVIS_PHP_VERSION" != "7.0" ]]; then
 	exit 0
 fi
 
-cd "$DIR"
-php "$ROOT"/vendor/bin/coveralls --verbose --config=coveralls.yml
+composer require satooshi/php-coveralls --prefer-dist --no-progress --no-interaction
